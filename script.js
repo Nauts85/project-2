63a11dbc5dcaca3903e6181f6999ae008673c04f
Nauts85 <117826382+Nauts85@users.noreply.github.com>
2023-06-23 10:28:24 +0000

    added event listener to index.html start button and gameIntro function to script.js.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//questions to use in the quiz
+// questions to use in the quiz.
 const questions = [
   {
   question:"What Color is the famous 1980s arcade character that likes to eat a varied diet of Pellets, Fruit and Ghosts?", 
@@ -21,16 +21,16 @@ const questions = [
   
 }]
 
-//document elements//
-const confirmButtonElement = document.getElementById('#submit-answer');//confirm answer button
-const answerOptionElement = document.getElementsByClassName('option');// option buttons
-const questionElement = document.getElementById('question');//<h1> question display element
+// document elements.
+const confirmButtonElement = document.getElementById('#submit-answer'); //confirm answer button.
+const answerOptionElement = document.getElementsByClassName('option'); // option buttons.
+const questionElement = document.getElementById('question'); //<h1> question display element.
 
-//question variables//
+// question variables.
 let currentQuestionIndex;
 let jumbledQuestions;
 
-$('#submit-answer').on('click', startGame) //TEMPORARY start button for testing purposes
+$('#submit-answer').on('click', startGame) //TEMPORARY start button for testing purposes.
 
 function startGame(){
   currentQuestionIndex = 0
@@ -39,14 +39,33 @@ function startGame(){
 }
 
 function getNextQuestion(){
-  displayQuestion(jumbledQuestions[currentQuestionIndex]) //defines question as questions[0].question
+  displayQuestion(jumbledQuestions[currentQuestionIndex]) // defines question as questions[0].question
 }
 
 function displayQuestion(question){
   questionElement.innerText = question.question 
 }
 
-
+/**
+ * Event handler for the start button on index.html
+ * each element is faded in, totalling 10 seconds per transition
+ * ending with the introduction to the first game section
+ */
+$("#btn-start").on("click", function gameIntro() {
+  $("#btn-start").fadeOut(1000);
+  setTimeout(() => {
+    $(".first-intro").fadeIn(2000).fadeOut(3000);
+  }, 5000);
+  setTimeout(() => {
+    $(".second-intro").fadeIn(2000).fadeOut(3000);
+  }, 10000);
+  setTimeout(() => {
+    $(".third-intro").fadeIn(1000);
+  }, 15000);
+  setTimeout(() => {
+    window.location.replace('game.html')
+  }, 22000);
+});
 
 //function displayNextQuestion()
 //displayOptions();
