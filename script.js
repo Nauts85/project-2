33ce852454324a561bd9c25b0bbb32e2f028d27e
Nauts85 <117826382+Nauts85@users.noreply.github.com>
2023-06-27 11:29:46 +0000

    added basic function to get and check selected answer to script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 const questions = [
   {
     question:
-      "What Color is the famous 1980s arcade character that likes to eat a varied diet of Pellets, Fruit and Ghosts?",
-    option1: "Green",
-    option2: "Blue",
-    option3: "Yellow",
-    option4: "Pink"
+      'What Color is the famous 1980s arcade character that likes to eat a varied diet of Pellets, Fruit and Ghosts?',
+    option1: 'Green',
+    option2: 'Blue',
+    option3: 'Yellow',
+    option4: 'Pink'
   },
   {
     question:
-      "What was the name of the popular Atarii game that involves shooting invading aliens from the sky while hiding behind 3 barriers?",
-    option1: "Space Invaders",
-    option2: "OMG Alien invasion.",
-    option3: "Angry Aliens from Mars.",
-    option4: "Galaxy Gankers"
+      'What was the name of the popular Atarii game that involves shooting invading aliens from the sky while hiding behind 3 barriers?',
+    option1: 'Space Invaders',
+    option2: 'OMG Alien invasion.',
+    option3: 'Angry Aliens from Mars.',
+    option4: 'Galaxy Gankers'
   }
 ];
 
@@ -22,29 +22,33 @@ const questions = [
 const confirmButtonElement = document.getElementById('#submit-answer'); 
 
 //Option buttons.
-const optionButtonOne = document.getElementById("option1");
-const optionButtonTwo = document.getElementById("option2");
-const optionButtonThree = document.getElementById("option3");
-const optionButtonFour = document.getElementById("option4");
+const optionButtonOne = document.getElementById('option1');
+const optionButtonTwo = document.getElementById('option2');
+const optionButtonThree = document.getElementById('option3');
+const optionButtonFour = document.getElementById('option4');
 //<h1> question display element.
 const questionElement = document.getElementById('question'); 
 
 // question variables.
 let currentQuestionIndex;
-let jumbledQuestions;
+let quizQuestions;
+
+//player score variable
+let playerScore = 0;
+
 
  //TEMPORARY start button for testing purposes.
 $('#submit-answer').on('click', startGame)
 
 function startGame(){
   currentQuestionIndex = 0
-  jumbledQuestions = questions.sort(() => Math.random() - .5)
+  quizQuestions = questions
   getNextQuestion()
 }
 
  // defines question as questions[0].question
 function getNextQuestion(){
-  displayQuestion(jumbledQuestions[currentQuestionIndex])
+  displayQuestion(quizQuestions[currentQuestionIndex])
 }
 
 function displayQuestion(question) {
@@ -55,22 +59,40 @@ function displayQuestion(question) {
   optionButtonFour.innerText = question.option4;
 }
 
+//check answer function.
+$("#opt1,#opt2,#opt3,#opt4").on("click", function getAnswer(answer) {
+  let selectedAnswer = this.innerText;
+  answer = quizQuestions[currentQuestionIndex].answer;
+  if (selectedAnswer === answer) {
+    console.log("Correct");
+  } else {
+    console.log("Nope!");
+    console.log(answer)
+    console.log(selectedAnswer)
+  }
+});
+
+//this is needed somewhere just not yet.
+//playerScore++;
+//playerScoreElement.innerText = playerScore
+
+
 
 /**
  * Event handler for the start button on index.html
  * each element is faded in, totalling 10 seconds per transition
  * ending with the introduction to the first game section
  */
-$("#btn-start").on("click", function gameIntro() {
-  $("#btn-start").fadeOut(1000);
+$('#btn-start').on('click', function gameIntro() {
+  $('#btn-start').fadeOut(1000);
   setTimeout(() => {
-    $(".first-intro").fadeIn(2000).fadeOut(3000);
+    $('.first-intro').fadeIn(2000).fadeOut(3000);
   }, 5000);
   setTimeout(() => {
-    $(".second-intro").fadeIn(2000).fadeOut(3000);
+    $('.second-intro').fadeIn(2000).fadeOut(3000);
   }, 10000);
   setTimeout(() => {
-    $(".third-intro").fadeIn(1000);
+    $('.third-intro').fadeIn(1000);
   }, 15000);
   setTimeout(() => {
     window.location.replace('game.html')
